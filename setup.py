0272097de2cbda50216b38fb6bc2051026ca1922
Andrew Halle <ahalle@berkeley.edu>
2018-04-02 10:39:03 -0700

    final updates to setup.py and LICENSE

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="sudoku_solver",
     version="0.1",
+    description="A sudoku solver using CSP techniques",
+    url="https://github.com/andrewhalle/sudoku_solver",
+    author="Andrew Halle",
+    author_email="ahalle@berkeley.edu",
+    license="MIT",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
